a7970e2960fe52a7db348808b3c44ac7eb3ed52d
Daniel Vargas <dvargas2013@gmail.com>
2020-01-14 17:06:17 -0500

    Time class adjustments, and removed bisectHrMnHands

--- done/Time.py
+++ done/Time.py
@@ -20,16 +20,14 @@ class Time:
             Time('8') - set hours from string
             Time('8:0') - set hours, minutes from string
             Time('8:0:0') - set hours, minutes, seconds from string
-        
-        If initialization fails will return a Time() instance with time of creation
+
+            Time() - returns an instance with time of creation
         """
-        try:
-            if len(time)==1:
-                if type(time[0])==str: new = time[0].split(":")
-                else: new = [self.run(time[0],0),0,0]
-            elif len(time)>1: new = [self.run(i,0) for i in time[:3]]
-            else: raise Exception()
-        except:
+        if len(time)==1:
+            if type(time[0])==str: new = time[0].split(":")
+            else: new = [self.run(time[0],0),0,0]
+        elif len(time)>1: new = [self.run(i,0) for i in time[:3]]
+        else:
             a=datetime.now()
             new=[a.hour,a.minute,a.second+a.microsecond/1e6]
         while len(new)<3: new=list(new)+[0]
@@ -39,31 +37,31 @@ class Time:
         self.mn=int(self.mn%60) #Reduce minutes
         self.sc=round(self.sc%60,9) #Reduce seconds
         #That's the best way I thought of at the moment
-    #Some numbers associated with the time
+    @property
     def totalSc(self):
-        """Calculate and return the total amount of seconds from midnite to time"""
+        """Total amount of seconds from midnite to time"""
         return 60*(60*self.hr+self.mn)+self.sc
+    @property
     def hrHandDeg(self):
-        """Calculate and return angle between the 12 and position of the hour hand"""
+        """Angle between the 12 and position of the hour hand"""
         return (60*(60*self.hr+self.mn)+self.sc)/120%360
+    @property
     def mnHandDeg(self):
-        """Calculate and return angle between the 12 and position of the minute hand"""
+        """Angle between the 12 and position of the minute hand"""
         return (60*self.mn+self.sc)/10%360
+    @property
     def scHandDeg(self):
-        """Calculate and return angle between the 12 and position of the second hand"""
+        """Angle between the 12 and position of the second hand"""
         return self.sc*6%360
-    #Some setting functions
-    def fromS(self,secs):
-        """Set the time according to how many seconds from midnite the time should be"""
-        self.hr,self.mn,self.sc = int((secs/120%360))//30,int((secs/10%360))//6,(secs*6%360)/6
-        if abs(self.sc%1) < 1e-9: self.sc=int(self.sc)+.0
-        return self
+    @hrHandDeg.setter
     def hrFromD(self,deg):
         """Set the hour hand of time to where it should be according to the angle given in degrees"""
         self.hr=deg//30
+    @mnHandDeg.setter
     def mnFromD(self,deg):
         """Set the minute hand of time to where it should be according to the angle given in degrees"""
         self.mn=deg//6
+    @scHandDeg.setter
     def scFromD(self,deg):
         """Set the second hand of time to where it should be according to the angle given in degrees"""
         self.sc=deg/6
@@ -82,10 +80,10 @@ class Time:
     #Some math operations for time
     def __add__(self, new): return Time(self.hr+new.hr,self.mn+new.mn,self.sc+new.sc)
     def __sub__(self, new): return Time(self.hr-new.hr,self.mn-new.mn,self.sc-new.sc)
-    def __mul__(self, new): return Time(0).fromS(self.totalSc()*self.run(new,1))
-    def __truediv__(self,new): return Time(0).fromS(self.totalSc()/self.run(new,1))
-    def __floordiv__(self,new): return Time(0).fromS(self.totalSc()//self.run(new,1))
-    def __mod__(self,new): return Time(0).fromS(self.totalSc()%self.run(new,self.totalSc()+1))
+    def __mul__(self, new): return Time(0,0,self.totalSc()*self.run(new,1))
+    def __truediv__(self,new): return Time(0,0,self.totalSc()/self.run(new,1))
+    def __floordiv__(self,new): return Time(0,0,self.totalSc()//self.run(new,1))
+    def __mod__(self,new): return Time(0,0,self.totalSc()%self.run(new,self.totalSc()+1))
     def __round__(self,n): return Time(self.hr,self.mn,round(self.sc,n))
     #Comparisons
     def __lt__(self,new):
@@ -100,29 +98,15 @@ class Time:
     def __gt__(self,new): return not self<=new
     def __ge__(self,new): return not self<new
     #Show the time in a cool way
-    def __repr__(self): return '{:0>2}:{:0>2}:{:0>2}'.format(self.hr,self.mn,self.sc)
-    def __str__(self): return self.__repr__()
-    def run(self,num,default=1):
+    def __repr__(self): return "Time(%s)" % self.__str__()
+    def __str__(self): return '{:0>2}:{:0>2}:{:0>2}'.format(self.hr,self.mn,self.sc)
+    def run(cls,num,default=1):
         """Convert parameter sent into the total amount of seconds.
         
         Can accept int, float, and Time. Anything else and default is returned"""
         if type(num)==type(self): return num.totalSc()
         if type(num) in [int, float]: return num
         return default
-
-def bisectHrMnHands(hr,mn):
-    """Calculate the time at which the second hand will bisect the hour and minute hands"""
-    h = int(hr) 
-    m = int(mn)
-    sDegMin = (60*h+m)/2
-    sDegMax = 6*m
-    while 1:
-        sDeg = (sDegMin+sDegMax)/2
-        hDeg = (60*h+m)/2 + sDeg/6/120
-        mDeg = 6*m + sDeg/60
-        if   2*sDeg== hDeg+mDeg : return '{:0>2}:{:0>2}:{:0>2}'.format(h,m,(sDeg%360)/6)
-        elif 2*sDeg < hDeg+mDeg : sDegMin = sDeg
-        else                    : sDegMax = sDeg
 
 def DayOfTheWeek(month,date,year):
     """"Calculate the Day of the Week according to month, day, year given."""
