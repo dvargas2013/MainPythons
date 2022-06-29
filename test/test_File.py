413aec8009b63b7fe0ffc41182344c2a9863be75
Daniel Vargas <dvargas2013@gmail.com>
2022-06-29 13:55:58 -0400

    TGF test more automated

--- test/test_File.py
+++ test/test_File.py
@@ -2,8 +2,10 @@ import os
 
 from done import File
 
+
 def test_homeDirectory():
     assert File.exists(File.getHome())
+
 
 def test_same():
     for file1, file2 in zip(
@@ -11,19 +13,32 @@ def test_same():
             File.files('.')):
         assert File.same(file1, file2)
 
+
 def test_cwdas():
     here = os.getcwd()
     with File.cwd_as(".."):
         assert here != os.getcwd()
     assert here == os.getcwd()
 
+
+def singleTGF_test(a, b):
+    assert a.nodes == b.nodes
+    assert sorted(a.ordered_nodes) == sorted(b.ordered_nodes)
+    assert sorted(a.ordered_nodemapping) == sorted(b.ordered_nodemapping)
+    assert a.to_dict() == b.to_dict()
+
+
 def test_TGF():
-    # TODO find simple way to make random networks to test TGF constraints
     a = File.TGF({"a": ["b", "c"]})
     b = File.TGF({"a": {"b", "c"}})
-    assert a.nodes == b.nodes
-    assert a.ordered_nodes == b.ordered_nodes
-    assert list(a.ordered_nodemapping) == list(b.ordered_nodemapping)
+    singleTGF_test(a, b)
 
-    assert str(a) == str(b)
-    assert a.to_dict() == b.to_dict()
+    from string import ascii_lowercase
+    from random import sample
+
+    nodes = list(ascii_lowercase)
+    for connections in range(1, 10):
+        network = {n: sample(ascii_lowercase, k=connections) for n in nodes}
+        network_with_set = {n: set(j) for n, j in network.items()}
+
+        singleTGF_test(File.TGF(network), File.TGF(network_with_set))
