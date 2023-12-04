de468a9594c24de56d77ca683ca1ee9fdb291a4c
Daniel Vargas <dvargas2013@gmail.com>
2023-12-03 19:54:58 -0800

    allow for submodules in packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='done',
     version='0.0.0',
-    packages=['done'],
+    packages=find_packages(),
     extras_require={
         'test': ['pytest']
     }
